07ca0fb969756e12a3f0f1b7d771442b6f073c34
louise-decombe <62133595+louise-decombe@users.noreply.github.com>
2020-06-08 17:15:17 +0200

    maj bdd

--- discussion.sql
+++ discussion.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2020 at 06:00 PM
+-- Generation Time: Jun 08, 2020 at 05:13 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,6 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(140) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=83 ;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `message`, `id_utilisateur`, `date`) VALUES
+(74, 'bonsoir', 9, '2020-06-08'),
+(78, 'comment allez-vous ?', 9, '2020-06-08'),
+(79, 'ça va bien merci ! et toi ?', 10, '2020-06-08'),
+(81, 'bien et vous ?', 10, '2020-06-08'),
+(82, 'vous venez ce soir ?', 10, '2020-06-08');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `utilisateurs`
 --
 
@@ -31,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `utilisateurs`
@@ -40,7 +65,8 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
 (4, 'franck', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),
 (6, 'marie', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5'),
-(7, 'sarah', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
+(9, 'julie', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5'),
+(10, 'maureen', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
